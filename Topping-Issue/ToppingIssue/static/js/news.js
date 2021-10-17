b21b0ddfea25a811e11b0d3831674787201fe8fd
ahyeon0508 <ahyeon_0508@naver.com>
2021-10-17 19:34:51 +0900

    feat, design: Add subword function and Change news page design
    Add subword function
    Change news page design : chart, news-info

--- Topping-Issue/ToppingIssue/static/js/news.js
+++ Topping-Issue/ToppingIssue/static/js/news.js
@@ -1,61 +1,24 @@
 // user-setting
-$(document).ready(function (){
-	$(".setting-button").click(function (){
-  	$("#date-setting").toggle();
+$(document).ready(function () {
+  $(".setting-button").click(function () {
+    $("#date-setting").toggle();
     $("#interest-setting").toggle();
     $("#complete-button").toggle();
   });
 });
 
 // 기간별 토픽 차트
-var keyword = [['관련어1', '관련어2'], ['관련어3', '관련어4']]
+var termChartDate = JSON.parse(termChartDate);
+var termChartsubData = JSON.parse(termChartsubData);
 
 $(document).ready(function () {
-  new Chart(document.getElementById("term-topic-chart"), {
+  var ctx = document.getElementById('term-topic-chart');
+  var config = {
     type: 'line',
     data: {
-			labels: [
-				'2021-04-24',
-				'2021-04-25',
-				'2021-04-26',
-				'2021-04-27',
-				'2021-04-28',
-				'2021-04-29',
-				'2021-04-30'
-			],
-			datasets: [{
-				label: 'My First dataset',
-				backgroundColor: 'rgba(75, 192, 192, 1)',
-				borderColor: 'rgba(75, 192, 192, 1)',
-				fill: false,
-        pointRadius: 3,
-				data: [
-					Math.floor(Math.random() * 50),
-					Math.floor(Math.random() * 50),
-					Math.floor(Math.random() * 50),
-					Math.floor(Math.random() * 50),
-					Math.floor(Math.random() * 50),
-					Math.floor(Math.random() * 50),
-					Math.floor(Math.random() * 50)
-				],
-			},
-      {
-				label: 'My Second dataset',
-				backgroundColor: 'rgba(255, 99, 132, 1)',
-				borderColor: 'rgba(255, 99, 132, 1)',
-				fill: false,
-        pointRadius: 3,
-        data: [
-					Math.floor(Math.random() * 50),
-					Math.floor(Math.random() * 50),
-					Math.floor(Math.random() * 50),
-					Math.floor(Math.random() * 50),
-					Math.floor(Math.random() * 50),
-					Math.floor(Math.random() * 50),
-					Math.floor(Math.random() * 50)
-				],
-			}]
-		},
+      labels: termChartDate,
+      datasets: []
+    },
     options: {
       responsive: true,
       maintainAspectRatio: false,
@@ -64,8 +27,9 @@ $(document).ready(function () {
       },
       title: {
         display: true,
-        text: '2021-04-24 ~ 2021-04-30',
-        position: 'bottom',
+        text: termChartDate[0] + " ~ " + termChartDate[termChartDate.length - 1],
+        position: 'top',
+        fontColor: "gray",
       },
       scales: {
         xAxes: [{
@@ -73,7 +37,7 @@ $(document).ready(function () {
             display: false
           },
           gridLines: {
-            display:false,
+            display: false,
           }
         }],
         yAxes: [{
@@ -82,180 +46,447 @@ $(document).ready(function () {
       },
       tooltips: {
         callbacks: {
-          label: function(tooltipItem, data) {
-            return data.datasets[tooltipItem.datasetIndex].label + ' : ' + keyword[tooltipItem.datasetIndex]
+          label: function (tooltipItem, data) {
+            console.log(termChartsubData[tooltipItem.index][tooltipItem.datasetIndex])
+            return "[" + data.datasets[tooltipItem.datasetIndex].label + "]"
+          },
+          afterLabel: function (tooltipItem) {
+            return termChartsubData[tooltipItem.index][tooltipItem.datasetIndex]
           }
         }
       }
     }
-  });
-});
+  };
 
-// N사, D사 감성 분석 차트
-var section = document.getElementsByClassName("news-reaction-chart");
+  var termTopicChart = new Chart(ctx, config);
 
-$(document).ready(function () {
-  for(var i = 0; i < section.length; i++){
-    if (JSON.stringify(JSON.parse(N_sentimentData)[i]) == JSON.stringify([0, 0, 0])){
-      new Chart(document.getElementsByClassName("naver-chart")[i], {
-        type: 'doughnut',
-        data: {
-          labels: ['긍정', '부정', '판별불가'],
-          datasets: [{
-            backgroundColor: ["#DEF2F3", "#F1B6AF","#EAE6E6"],
-            data: JSON.parse(N_sentimentData)[i]
-          }]
-        },
-        options: {
-          responsive: true,
-          maintainAspectRatio: false,
-          legend: {
-            display: false
-          },
-          plugins: {
-            doughnutlabel: {
-              labels: [
-                {
-                  text: 'N사',
-                  font: {
-                    size: '36',
-                    weight: 'bold'
-                  },
-                },
-                {
-                  text: '- 관련 댓글이',
-                  font: {
-                    size: '18',
-                    weight: 'bold'
-                  },
-                },
-                {
-                  text: '존재하지 않습니다 -',
-                  font: {
-                    size: '18',
-                    weight: 'bold'
-                  },
-                },
-              ],
-            },
-          },
-        }
-      });
+  var color = ['rgba(185, 176, 255, 1)', 'rgba(255, 216, 176, 1)', 'rgba(255, 176, 176, 1)', 'rgba(176, 255, 178, 1)', 'rgba(176, 255, 249, 1)'];
+
+  var keywords = Object.keys(JSON.parse(termChartData));
+  for (var i = 0; i < keywords.length; i++) {
+    var term_dataset = {
+      label: [],
+      borderColor: '',
+      backgroundColor: '',
+      data: [],
+      fill: false
     }
-    else{
-      new Chart(document.getElementsByClassName("naver-chart")[i], {
-        type: 'doughnut',
-        data: {
-          labels: ['긍정', '부정', '판별불가'],
-          datasets: [{
-            backgroundColor: ["#DEF2F3", "#F1B6AF","#EAE6E6"],
-            data: JSON.parse(N_sentimentData)[i]
-          }]
-        },
-        options: {
-          responsive: true,
-          maintainAspectRatio: false,
-          legend: {
-            display: false
-          },
-          plugins: {
-            doughnutlabel: {
-              labels: [
-                {
-                  text: 'N사',
-                  font: {
-                    size: '16',
-                    weight: 'bold'
-                  },
-                },
-              ],
-            },
-          },
-        }
-      });
-    }
-    
-    if (JSON.stringify(JSON.parse(D_sentimentData)[i]) == JSON.stringify([0, 0, 0])){
-      new Chart(document.getElementsByClassName("daum-chart")[i], {
-        type: 'doughnut',
-        data: {
-          labels: ['긍정', '부정', '판별불가'],
-          datasets: [{
-            backgroundColor: ["#DEF2F3", "#F1B6AF","#EAE6E6"],
-            data: JSON.parse(D_sentimentData)[i]
-          }]
-        },
-        options: {
-          responsive: true,
-          maintainAspectRatio: false,
-          legend: {
-            display: false
-          },
-          plugins: {
-            doughnutlabel: {
-              labels: [
-                {
-                  text: 'D사',
-                  font: {
-                    size: '36',
-                    weight: 'bold'
-                  },
-                },
-                {
-                  text: '- 관련 댓글이',
-                  font: {
-                    size: '18',
-                    weight: 'bold'
-                  },
-                },
-                {
-                  text: '존재하지 않습니다 -',
-                  font: {
-                    size: '18',
-                    weight: 'bold'
-                  },
-                },
-              ],
-            },
-          },
-        }
-      });
-    }
-    else{
-      new Chart(document.getElementsByClassName("daum-chart")[i], {
-        type: 'doughnut',
-        data: {
-          labels: ['긍정', '부정', '판별불가'],
-          datasets: [{
-            backgroundColor: ["#DEF2F3", "#F1B6AF","#EAE6E6"],
-            data: JSON.parse(D_sentimentData)[i]
-          }]
-        },
-        options: {
-          responsive: true,
-          maintainAspectRatio: false,
-          legend: {
-            display: false
-          },
-          plugins: {
-            doughnutlabel: {
-              labels: [
-                {
-                  text: 'D사',
-                  font: {
-                    size: '16',
-                    weight: 'bold'
-                  },
-                },
-              ],
-            },
-          },
-        }
-      });
-    }
-    
+
+    term_dataset.borderColor = color[i];
+    term_dataset.backgroundColor = color[i];
+    term_dataset.label = keywords[i];
+    term_dataset.data = JSON.parse(termChartData)[keywords[i]];
+
+    config.data.datasets.push(term_dataset);
+    termTopicChart.update();
   }
 });
 
+// N사, D사 감성 분석 차트
+var newsData = JSON.parse(newsData);
+var newsData_keys = Object.keys(newsData);
+var html = '';
 
+function news_info(key){
+  temp_html = '';
+  for (var i = 0; i < newsData[key].length; i++){
+    temp_html += '<div class="news-title" OnClick="location.href =\'' + newsData[key][1][i] + '\'">' + newsData[key][0][i] + '</div>'
+  }
+  return temp_html
+}
 
+function sub_news_info(m){
+  temp_html = '<div class=sub-word-buttons>';
+  for (var i = 0; i < subNewsData[m][0].length; i++){
+    temp_html += '<button class="sub-word-button">' + subNewsData[m][0][i] + '</button>'
+  }
+  temp_html += '</div>'
+  for (var i = 0; i < subNewsData[m][0].length; i++){
+    temp_html +=  '<div class="sub-news-keyword"><div class="sub-news"><div class="sub-news-info"><p class="cluster-name">'+ subNewsData[m][0][i] + '</p>'
+    for (var j = 0; j < subNewsData[m][1][i].length; j++){
+      temp_html += '<div class="news-title" OnClick="location.href =\'' + subNewsData[m][2][i][j] + '\'">' + subNewsData[m][1][i][j] + '</div>'
+    }
+    temp_html += '</div><div class="sub-news-reaction-chart">' +
+    '<div class="naver-news-reaction"><div class="naver"><canvas class="naver-subNews-chart"></canvas></div></div>' + 
+    '<div class="daum-news-reaction"><div class="daum"><canvas class="daum-subNews-chart"></canvas></div></div>'
+    temp_html += '</div></div></div></div>'
+  }
+  return temp_html
+}
+
+function news_chart(m){
+  if (JSON.stringify(JSON.parse(N_sentimentData)[m-1]) == JSON.stringify([0, 0, 0])){
+    new Chart(document.getElementsByClassName("naver-chart"), {
+      type: 'doughnut',
+      data: {
+        labels: ['부정', '긍정', '판별불가'],
+        datasets: [{
+          backgroundColor: ["#F1B6AF", "#DEF2F3", "#EAE6E6"],
+          data: JSON.parse(N_sentimentData)[m-1]
+        }]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        legend: {
+          display: false
+        },
+        plugins: {
+          doughnutlabel: {
+            labels: [
+              {
+                text: 'N사',
+                font: {
+                  size: '36',
+                  weight: 'bold'
+                },
+              },
+              {
+                text: '- 관련 댓글이',
+                font: {
+                  size: '18',
+                  weight: 'bold'
+                },
+              },
+              {
+                text: '존재하지 않습니다 -',
+                font: {
+                  size: '18',
+                  weight: 'bold'
+                },
+              },
+            ],
+          },
+        }
+      }
+    })
+  }
+  else{
+    new Chart(document.getElementsByClassName("naver-chart"), {
+      type: 'doughnut',
+      data: {
+        labels: ['부정', '긍정', '판별불가'],
+        datasets: [{
+          backgroundColor: ["#F1B6AF", "#DEF2F3", "#EAE6E6"],
+          data: JSON.parse(N_sentimentData)[m-1]
+        }]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        legend: {
+          display: false
+        },
+        plugins: {
+          doughnutlabel: {
+            labels: [
+              {
+                text: 'N사',
+                font: {
+                  size: '16',
+                  weight: 'bold'
+                },
+              },
+            ],
+          },
+        },
+      }
+    });
+  }
+  if (JSON.stringify(JSON.parse(D_sentimentData)[m-1]) == JSON.stringify([0, 0, 0])){
+    new Chart(document.getElementsByClassName("daum-chart"), {
+      type: 'doughnut',
+      data: {
+        labels: ['부정', '긍정', '판별불가'],
+        datasets: [{
+          backgroundColor: ["#F1B6AF", "#DEF2F3", "#EAE6E6"],
+          data: JSON.parse(D_sentimentData)[m-1]
+        }]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        legend: {
+          display: false
+        },
+        plugins: {
+          doughnutlabel: {
+            labels: [
+              {
+                text: 'D사',
+                font: {
+                  size: '36',
+                  weight: 'bold'
+                },
+              },
+              {
+                text: '- 관련 댓글이',
+                font: {
+                  size: '18',
+                  weight: 'bold'
+                },
+              },
+              {
+                text: '존재하지 않습니다 -',
+                font: {
+                  size: '18',
+                  weight: 'bold'
+                },
+              },
+            ],
+          },
+        }
+      }
+    })
+  }
+  else{
+    new Chart(document.getElementsByClassName("daum-chart"), {
+      type: 'doughnut',
+      data: {
+        labels: ['부정', '긍정', '판별불가'],
+        datasets: [{
+          backgroundColor: ["#F1B6AF", "#DEF2F3", "#EAE6E6"],
+          data: JSON.parse(D_sentimentData)[m-1]
+        }]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        legend: {
+          display: false
+        },
+        plugins: {
+          doughnutlabel: {
+            labels: [
+              {
+                text: 'D사',
+                font: {
+                  size: '16',
+                  weight: 'bold'
+                },
+              },
+            ],
+          },
+        },
+      }
+    });
+  }
+}
+
+function sub_news_chart(i, j){
+  if (JSON.stringify(JSON.parse(N_sub_sentimentData)[i][j]) == JSON.stringify([0, 0, 0])) {
+    if (window.n_chartObj != undefined){
+      window.n_chartObj.destroy();
+    }
+    window.n_chartObj = new Chart(document.getElementsByClassName("naver-subNews-chart")[j], {
+      type: 'doughnut',
+      data: {
+        labels: ['부정', '긍정', '판별불가'],
+        datasets: [{
+          backgroundColor: ["#F1B6AF", "#DEF2F3", "#EAE6E6"],
+          data: JSON.parse(N_sub_sentimentData)[i][j]
+        }]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        legend: {
+          display: false
+        },
+        plugins: {
+          doughnutlabel: {
+            labels: [
+              {
+                text: 'N사',
+                font: {
+                  size: '36',
+                  weight: 'bold'
+                },
+              },
+              {
+                text: '- 관련 댓글이',
+                font: {
+                  size: '18',
+                  weight: 'bold'
+                },
+              },
+              {
+                text: '존재하지 않습니다 -',
+                font: {
+                  size: '18',
+                  weight: 'bold'
+                },
+              },
+            ],
+          },
+        }
+      }
+    })
+  }
+  else{
+    if (window.n_chartObj != undefined){
+      window.n_chartObj.destroy();
+    }
+    window.n_chartObj = new Chart(document.getElementsByClassName("naver-subNews-chart")[j], {
+      type: 'doughnut',
+      data: {
+        labels: ['부정', '긍정', '판별불가'],
+        datasets: [{
+          backgroundColor: ["#F1B6AF", "#DEF2F3", "#EAE6E6"],
+          data: JSON.parse(N_sub_sentimentData)[i][j]
+        }]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        legend: {
+          display: false
+        },
+        plugins: {
+          doughnutlabel: {
+            labels: [
+              {
+                text: 'N사',
+                font: {
+                  size: '16',
+                  weight: 'bold'
+                },
+              },
+            ],
+          },
+        },
+      }
+    });
+  }
+  if (JSON.stringify(JSON.parse(D_sub_sentimentData)[i][j]) == JSON.stringify([0, 0, 0])) {
+    if (window.d_chartObj != undefined){
+      window.d_chartObj.destroy();
+    }
+    window.d_chartObj = new Chart(document.getElementsByClassName("daum-subNews-chart")[j], {
+      type: 'doughnut',
+      data: {
+        labels: ['부정', '긍정', '판별불가'],
+        datasets: [{
+          backgroundColor: ["#F1B6AF", "#DEF2F3", "#EAE6E6"],
+          data: JSON.parse(D_sub_sentimentData)[i][j]
+        }]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        legend: {
+          display: false
+        },
+        plugins: {
+          doughnutlabel: {
+            labels: [
+              {
+                text: 'N사',
+                font: {
+                  size: '36',
+                  weight: 'bold'
+                },
+              },
+              {
+                text: '- 관련 댓글이',
+                font: {
+                  size: '18',
+                  weight: 'bold'
+                },
+              },
+              {
+                text: '존재하지 않습니다 -',
+                font: {
+                  size: '18',
+                  weight: 'bold'
+                },
+              },
+            ],
+          },
+        }
+      }
+    })
+  }
+  else{
+    if (window.d_chartObj != undefined){
+      window.d_chartObj.destroy();
+    }
+    window.d_chartObj = new Chart(document.getElementsByClassName("daum-subNews-chart")[j], {
+      type: 'doughnut',
+      data: {
+        labels: ['부정', '긍정', '판별불가'],
+        datasets: [{
+          backgroundColor: ["#F1B6AF", "#DEF2F3", "#EAE6E6"],
+          data: JSON.parse(D_sub_sentimentData)[i][j]
+        }]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        legend: {
+          display: false
+        },
+        plugins: {
+          doughnutlabel: {
+            labels: [
+              {
+                text: 'D사',
+                font: {
+                  size: '16',
+                  weight: 'bold'
+                },
+              },
+            ],
+          },
+        },
+      }
+    });
+  }
+  window.n_chartObj.canvas.parentNode.style.width = '250px';
+  window.n_chartObj.canvas.parentNode.style.height = '125px';
+  window.d_chartObj.canvas.parentNode.style.width = '250px';
+  window.d_chartObj.canvas.parentNode.style.height = '125px';
+}
+
+function fnMove(){
+  var offset = $(".content").offset();
+  $('html, body').animate({scrollTop : offset.top}, 400);
+}
+
+$(document).ready(function () {
+  $(".content").hide()
+  for (var i = 0; i < 11; i++) {
+    (function (m) {
+      $("#top-news-" + (m)).click(function () {
+        html = '';
+        $(".content").empty();
+        $(".content").hide()
+        html += '<div class="news-info"><p class="cluster-name">' + newsData_keys[m-1] + '</p>'
+        html += news_info(newsData_keys[m-1]) + '</div>'
+        html += '<div class="news-reaction-chart">' + 
+        '<div class="naver-news-reaction"><div class="naver"><canvas class="naver-chart"></canvas></div></div>' + 
+        '<div class="daum-news-reaction"><div class="daum"><canvas class="daum-chart"></canvas></div></div></div>'
+        html += sub_news_info(m-1)
+        if ($(".content").is(':empty')){
+          $(".content").toggle()
+          $(".content").append(html);
+          news_chart(m);
+          $(".sub-news-keyword").hide();
+          $(".sub-word-button").click(function () {
+            if ($(this).index() == 0){
+              $('.sub-news-keyword').eq(1).hide();
+              $('.sub-news-keyword').eq(0).toggle();
+              sub_news_chart(m-1, $(this).index())
+            }
+            else {
+              $('.sub-news-keyword').eq(0).hide();
+              $('.sub-news-keyword').eq(1).toggle();
+              sub_news_chart(m-1, $(this).index())
+            }
+          });
+          fnMove()
+        }
+      });
+    })(i);
+  }
+});
--- Topping-Issue/ToppingIssue/views/news_views.py
+++ Topping-Issue/ToppingIssue/views/news_views.py
@@ -6,6 +6,13 @@ from ast import literal_eval
 import json
 from numpyencoder import NumpyEncoder
 
+def min_max(data):
+    if len(data) == 1:
+        return data
+    else:
+        x_min, x_max = min(data), max(data)
+        return (data-x_min)/(x_max-x_min)
+
 @app.route('/news', methods = ['GET'])
 def news():
     selectCategory = request.args.get('subCategory')
@@ -54,15 +61,98 @@ def news():
     news_df['Title'] = news_df['Title'].apply(lambda x: literal_eval(x))
     news_df['Link'] = news_df['Link'].apply(lambda x: literal_eval(x))
 
-    newsData = dict()
-    N_sentimentData = list()
-    D_sentimentData = list()
+    # keyword, sub_keyword
+    news_df.loc[news_df['key_word'] == '0', 'key_word'] = 'NA'
+    news_df.loc[news_df['sub_words'] == '0', 'sub_words'] = 'NA'
 
-    for i in range(0, len(user_dict)):
-        newsData[news_df['key_word'][i]] = [news_df['Title'][i][0:3], news_df['Link'][i][0:3]]
-        N_sentimentData.append([news_df['N_Good'][i], news_df['N_Bad'][i], news_df['N_Neut'][i]])
-        D_sentimentData.append([news_df['D_Good'][i], news_df['D_Bad'][i], news_df['D_Neut'][i]])
+    key_word_list = list(news_df['key_word'].unique())
+    key_word_list.remove('NA')
 
-    return render_template("news.html", newsData=newsData, subCategory='하이',
-        N_sentimentData=json.dumps(N_sentimentData, cls=NumpyEncoder), D_sentimentData=json.dumps(D_sentimentData, cls=NumpyEncoder))
+    user_dict = dict()
+    score_dict = dict()
+    for key_word in key_word_list:
+        news_df['Rank_score'] = ap*(min_max(news_df['Article_cnt'])) + up*(min_max(news_df['Comment_cnt']) + min_max(news_df['Sentiment_cnt']))/2
+        temp_df = news_df.loc[news_df['key_word'] == key_word,:].copy()
+        temp_df['Sub_rank'] = list(ap*((min_max(temp_df['Article_cnt']))) + up*(min_max(temp_df['Comment_cnt']) + min_max(temp_df['Sentiment_cnt']))/2)
+        temp_df.sort_values(by = ['Sub_rank'], ascending = False, inplace = True)
+        user_dict[key_word] = temp_df.copy()
+        score_dict[key_word] = user_dict[key_word].Rank_score.sum()
 
+    score_dict = sorted(score_dict.items(), key=lambda x : x[1], reverse = True)
+
+    # 전송 데이터
+    newsName_list, news_dict, subNews_list = list(), dict(), list()
+    N_sentiment_list, D_sentiment_list = list(), list()
+    N_sub_sentiment_list, D_sub_sentiment_list = list(), list()
+
+    term_chart_df = pd.DataFrame(columns = ['DATE', 'key_word', 'sub_words', 'Rank_score', 'Sub_rank'])
+
+    for i in range(10):
+        temp_news_title, temp_news_link, temp_N_sentiment, temp_D_sentiment = list(), list(), list(), list()
+        temp_subnews_title, temp_subnews_link, temp_N_sub_sentiment, temp_D_sub_sentiment, temp_subwords = list(), list(), list(), list(), list()
+
+        user_dict[score_dict[i][0]].reset_index(drop=True, inplace=True)
+
+        # term-chart-df
+        if i < 5:
+            term_chart_df = term_chart_df.append(user_dict[score_dict[i][0]][['DATE', 'key_word', 'sub_words', 'Rank_score', 'Sub_rank']], ignore_index=True)
+
+        for j in range(len(user_dict[score_dict[i][0]])):
+            if user_dict[score_dict[i][0]].loc[j]['sub_words'] == 'NA':
+                temp_news_title.append(user_dict[score_dict[i][0]].loc[j]['Title'][:3])
+                temp_news_link.append(user_dict[score_dict[i][0]].loc[j]['Link'][:3])
+                temp_N_sentiment.append([user_dict[score_dict[i][0]].loc[j]['N_Good'], user_dict[score_dict[i][0]].loc[j]['N_Bad'], user_dict[score_dict[i][0]].loc[j]['N_Neut']])
+                temp_D_sentiment.append([user_dict[score_dict[i][0]].loc[j]['D_Good'], user_dict[score_dict[i][0]].loc[j]['D_Bad'], user_dict[score_dict[i][0]].loc[j]['D_Neut']])
+            else:
+                temp_subnews_title.append(user_dict[score_dict[i][0]].loc[j]['Title'][:4])
+                temp_subnews_link.append(user_dict[score_dict[i][0]].loc[j]['Link'][:4])
+                temp_N_sub_sentiment.append([user_dict[score_dict[i][0]].loc[j]['N_Good'], user_dict[score_dict[i][0]].loc[j]['N_Bad'], user_dict[score_dict[i][0]].loc[j]['N_Neut']])
+                temp_D_sub_sentiment.append([user_dict[score_dict[i][0]].loc[j]['D_Good'], user_dict[score_dict[i][0]].loc[j]['D_Bad'], user_dict[score_dict[i][0]].loc[j]['D_Neut']])
+                temp_subwords.append('# ' + user_dict[score_dict[i][0]].loc[j]['sub_words'])
+
+        # 모두 sub_word가 있는 경우
+        if len(temp_news_title) == 0:
+            newsName_list.append(score_dict[i][0])
+            news_dict[score_dict[i][0]] = [temp_subnews_title[0][:2], temp_subnews_link[0][:2]]
+            subNews_list.append([temp_subwords[1:3], temp_subnews_title[1:3], temp_subnews_link[1:3]])
+            N_sentiment_list.append(temp_N_sub_sentiment[0])
+            D_sentiment_list.append(temp_D_sub_sentiment[0])
+            N_sub_sentiment_list.append(temp_N_sub_sentiment[1:3])
+            D_sub_sentiment_list.append(temp_D_sub_sentiment[1:3])
+
+        else:
+            newsName_list.append(score_dict[i][0])
+            news_dict[score_dict[i][0]] = [temp_news_title[0], temp_news_link[0]]
+            subNews_list.append([temp_subwords[:2], temp_subnews_title[:2], temp_subnews_link[:2]])
+            N_sentiment_list.append(temp_N_sentiment[0])
+            D_sentiment_list.append(temp_D_sentiment[0])
+            N_sub_sentiment_list.append(temp_N_sub_sentiment[:2])
+            D_sub_sentiment_list.append(temp_N_sub_sentiment[:2])
+
+    # 기간에 따른 핵심 키워드 추출 기능
+    termChartDate = pd.date_range(start = startDate, end = endDate).strftime("%Y-%m-%d").tolist()
+
+    term_keyword_list = term_chart_df['key_word'].unique()
+    termChartData = dict()
+    termChartsubData = list()
+
+    for keyword in term_keyword_list:
+        termChartData[keyword] = []
+
+    for date in termChartDate:
+        temp_chart_df = term_chart_df[term_chart_df['DATE'] == date]
+        temp_chart_df = temp_chart_df[temp_chart_df['sub_words'] != "NA"]
+        temp_sub_word = list()
+        for keyword in term_keyword_list:
+            temp_score_list = termChartData.get(keyword)
+
+            if len(temp_chart_df[temp_chart_df['key_word'] == keyword]['Sub_rank'].to_list()) == 0:
+                temp_score_list.append('')
+            else:
+                temp_score_list.append(temp_chart_df[temp_chart_df['key_word'] == keyword]['Sub_rank'].to_list()[0])
+            temp_sub_word.append(temp_chart_df[temp_chart_df['key_word'] == keyword]['sub_words'].to_list()[:2])
+        termChartsubData.append(temp_sub_word)
+
+    return render_template("news.html", newsName=newsName_list, newsData=json.dumps(news_dict, ensure_ascii=False), subNewsData=subNews_list, termChartData=json.dumps(termChartData, ensure_ascii=False), termChartDate=json.dumps(termChartDate), termChartsubData=json.dumps(termChartsubData),
+        N_sentimentData=json.dumps(N_sentiment_list, cls=NumpyEncoder), D_sentimentData=json.dumps(D_sentiment_list, cls=NumpyEncoder),
+        N_sub_sentimentData=json.dumps(N_sub_sentiment_list, cls=NumpyEncoder), D_sub_sentimentData=json.dumps(D_sub_sentiment_list, cls=NumpyEncoder), enumerate=enumerate)
